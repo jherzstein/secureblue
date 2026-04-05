83d8d190b34ee5621e77873b33af7341fdc3689f
Jordan Herzstein <63795875+jherzstein@users.noreply.github.com>
2026-04-05 14:20:20 -0400

    fix: sound.py proper on/off + include modules dir to r/w paths list

--- files/system/usr/libexec/secureblue/inner/sound.py
+++ files/system/usr/libexec/secureblue/inner/sound.py
@@ -23,36 +23,36 @@ def main() -> int:
     if len(sys.argv) != required_args_count:
         return 1
 
-    uname = subprocess.run(["uname", "-r"], check=True, text=True, capture_output=True)
-    kernel = uname.stdout.strip()
-    find = subprocess.Popen(
-        ["find", f"/lib/modules/{kernel}/kernel/sound", "-type", "f", "-name", "*.ko.xz"],
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-    sed = subprocess.Popen(
-        ["sed", "s/.*\\//install /; s/\\.ko\\.xz/ \\/bin\\/false/"],
-        stdin=find.stdout,
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-
-    find.stdout.close()
-
-    out = sed.communicate()
-
-    SND_MOD_TEXT = out[0]
 
     mode = sys.argv[1]
     match mode:
         case "on":
-            with open(SND_MOD_FILE, "w", encoding="utf8") as fd:
-                fd.write(SND_MOD_TEXT)
-            os.chmod(SND_MOD_FILE, 0o644)
+            os.remove(SND_MOD_FILE)
             print("Sound has been enabled. Reboot for effect.")
             return 0
         case "off":
-            os.remove(SND_MOD_FILE)
+            uname = subprocess.run(["uname", "-r"], check=True, text=True, capture_output=True)
+            kernel = uname.stdout.strip()
+            find = subprocess.Popen(
+                ["find", f"/lib/modules/{kernel}/kernel/sound", "-type", "f", "-name", "*.ko.xz"],
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+            sed = subprocess.Popen(
+                ["sed", "s/.*\\//install /; s/\\.ko\\.xz/ \\/bin\\/false/"],
+                stdin=find.stdout,
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+
+            find.stdout.close()
+
+            out = sed.communicate()
+
+            SND_MOD_TEXT = out[0]
+            with open(SND_MOD_FILE, "w", encoding="utf8") as fd:
+                fd.write(SND_MOD_TEXT)
+            os.chmod(SND_MOD_FILE, 0o644)
             print("Sound has been disabled. Reboot for effect.")
             return 0
         case _:
