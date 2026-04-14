66dcccc1ac4c54695b1aeb05ff636ac74c3fe1a9
Jordan Herzstein <63795875+jherzstein@users.noreply.github.com>
2026-04-14 19:45:28 -0400

    Revert "fix: Look in /proc/modules instead of lib/modules/ for blacklist."
    This reverts commit 52b76243cf808e159bbef17bfc55eb4e5a7f92ef.

--- files/system/usr/libexec/secureblue/inner/sound.py
+++ files/system/usr/libexec/secureblue/inner/sound.py
@@ -31,19 +31,27 @@ def main() -> int:
             print("Sound has been enabled. Reboot for effect.")
             return 0
         case "off":
-
-            grep = subprocess.run(
-                ["grep", "-oE", r'^(snd|sound|ac97|chv3-i2s|audio)[^ ]*', "/proc/modules"],
-                capture_output=True,
-                check=True,
+            uname = subprocess.run(["uname", "-r"], check=True, text=True, capture_output=True)
+            kernel = uname.stdout.strip()
+            find = subprocess.Popen(
+                ["find", f"/lib/modules/{kernel}/kernel/sound", "-type", "f", "-name", "*.ko.xz"],
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+            sed = subprocess.Popen(
+                ["sed", "s/.*\\//install /; s/\\.ko\\.xz/ \\/bin\\/false/"],
+                stdin=find.stdout,
+                stdout=subprocess.PIPE,
                 text=True,
             )
 
-            module_list = grep.stdout.splitlines()
+            find.stdout.close()
 
+            out = sed.communicate()
+
+            SND_MOD_TEXT = out[0]
             with open(SND_MOD_FILE, "w", encoding="utf8") as fd:
-                for module in module_list:
-                    fd.write(f"install {module} /bin/false\n")
+                fd.write(SND_MOD_TEXT)
             os.chmod(SND_MOD_FILE, 0o644)
             print("Sound has been disabled. Reboot for effect.")
             return 0
