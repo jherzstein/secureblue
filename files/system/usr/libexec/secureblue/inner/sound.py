b3b545ba7a51d22d658bd93e3a5bfc8a97ae6d82
Jordan Herzstein <63795875+jherzstein@users.noreply.github.com>
2026-04-14 20:18:46 -0400

    fix: get rid of subprocess from inner/sound.py

--- files/system/usr/libexec/secureblue/inner/sound.py
+++ files/system/usr/libexec/secureblue/inner/sound.py
@@ -10,7 +10,6 @@ The sandboxed sound toggle function
 
 import os
 import sys
-import subprocess
 from typing import Final
 
 SND_MOD_FILE: Final[str] = "/etc/modprobe.d/99-sound.conf"
@@ -31,25 +30,19 @@ def main() -> int:
             print("Sound has been enabled. Reboot for effect.")
             return 0
         case "off":
-            uname = subprocess.run(["uname", "-r"], check=True, text=True, capture_output=True)
-            kernel = uname.stdout.strip()
-            find = subprocess.Popen(
-                ["find", f"/lib/modules/{kernel}/kernel/sound", "-type", "f", "-name", "*.ko.xz"],
-                stdout=subprocess.PIPE,
-                text=True,
-            )
-            sed = subprocess.Popen(
-                ["sed", "s/.*\\//install /; s/\\.ko\\.xz/ \\/bin\\/false/"],
-                stdin=find.stdout,
-                stdout=subprocess.PIPE,
-                text=True,
-            )
+            kernel = os.uname().release
+            root_dir = f"/lib/modules/{kernel}/kernel/sound"
+            unwanted_modules = []
 
-            find.stdout.close()
+            for dirpath, dirnames, filenames in os.walk(root_dir):
+                for f in filenames:
+                    if f.endswith(".ko.xz"):
+                        unwanted_modules.append(os.path.basename(f).split('.',1)[0])
 
-            out = sed.communicate()
+            for module in unwanted_modules:
+                module = module.replace("-", "_")
+                SND_MOD_TEXT += f"install {module} /bin/false\n"
 
-            SND_MOD_TEXT = out[0]
             with open(SND_MOD_FILE, "w", encoding="utf8") as fd:
                 fd.write(SND_MOD_TEXT)
             os.chmod(SND_MOD_FILE, 0o644)
